341fb62321d23870dad44eb8eadacb682b4adfbc
syarotokki <maikuratokki@gmail.com>
2025-05-21 17:35:45 +0900

    Update youtube.py

--- utils/youtube.py
+++ utils/youtube.py
@@ -1,41 +1,67 @@
-import requests
 import os
+import requests
 
 YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
 
-def fetch_past_videos(channel_id, max_results=500):
-    base_url = "https://www.googleapis.com/youtube/v3/search"
+def fetch_latest_video(channel_id):
+    url = "https://www.googleapis.com/youtube/v3/search"
+    params = {
+        "part": "snippet",
+        "channelId": channel_id,
+        "maxResults": 1,
+        "order": "date",
+        "type": "video",
+        "key": YOUTUBE_API_KEY,
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    if "items" not in data or not data["items"]:
+        return None
+
+    item = data["items"][0]
+    video_id = item["id"]["videoId"]
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    return {
+        "url": video_url,
+        "title": item["snippet"]["title"],
+        "publishedAt": item["snippet"]["publishedAt"],
+        "is_live": item["snippet"].get("liveBroadcastContent") == "live"
+    }
+
+def fetch_past_videos(channel_id):
     videos = []
-    page_token = None
-    total_fetched = 0
+    next_page_token = None
 
     while True:
+        url = "https://www.googleapis.com/youtube/v3/search"
         params = {
             "part": "snippet",
             "channelId": channel_id,
             "maxResults": 50,
             "order": "date",
             "type": "video",
-            "key": YOUTUBE_API_KEY
+            "key": YOUTUBE_API_KEY,
         }
-        if page_token:
-            params["pageToken"] = page_token
+        if next_page_token:
+            params["pageToken"] = next_page_token
 
-        response = requests.get(base_url, params=params)
-        if response.status_code != 200:
+        response = requests.get(url, params=params)
+        data = response.json()
+
+        if "items" not in data:
             break
 
-        data = response.json()
-        for item in data.get("items", []):
+        for item in data["items"]:
             video_id = item["id"]["videoId"]
             video_url = f"https://www.youtube.com/watch?v={video_id}"
             videos.append(video_url)
-            total_fetched += 1
-            if total_fetched >= max_results:
-                return videos
 
-        page_token = data.get("nextPageToken")
-        if not page_token:
+        next_page_token = data.get("nextPageToken")
+        if not next_page_token:
             break
 
-    return videos
+    # 重複排除 & 昇順に（古い順に）
+    return list(dict.fromkeys(videos[::-1]))
